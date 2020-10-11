c3981abd74026077cf158aaa3cef5f88c8e6beef
Ipsit <mmkipsit@gmail.com>
2020-10-11 21:12:58 +0530

    fixed typo in the file output.csv

--- titanic/grader.py
+++ titanic/grader.py
@@ -7,7 +7,7 @@ def grade():
     parser.add_argument("--path", help="Give the path of the file to be graded")
     args = parser.parse_args()
     data_to_be_graded = pd.read_csv(os.path.join(os.getcwd(), args.path))
-    expected = pd.read_csv('./ouput.csv')
+    expected = pd.read_csv('./output.csv')
     accuracy = sum(expected['Survived'].values == data_to_be_graded['Survived'].values) / len(expected)
     print("Your accuracy is {}%".format(round(accuracy * 100, 2)))
 
